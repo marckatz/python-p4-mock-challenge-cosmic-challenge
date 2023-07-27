20caada7ed949bdd2e90177f8a728bd517e100ea
Marc Katz <marc.f.katz@gmail.com>
2023-07-27 15:38:54 -0400

    complete scientist stuff

--- server/app.py
+++ server/app.py
@@ -20,10 +20,67 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
-@app.route('/')
-def home():
-    return ''
+class Scientists(Resource):
+    def get(self):
+        scientists = [sci.to_dict(rules=('-missions',)) for sci in Scientist.query.all()]
+        return make_response(scientists,200)
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            new_scientist = Scientist(
+                name = data['name'],
+                field_of_study = data['field_of_study']
+            )
+            db.session.add(new_scientist)
+            db.session.commit()
+
+            return make_response(new_scientist.to_dict(),201)
+        except:
+            return make_response({"errors": ["validation errors"]}, 400)
+    
+api.add_resource(Scientists, '/scientists')
+
+class ScientistsById(Resource):
+    def get(self, id):
+        try:
+            scientist = Scientist.query.filter_by(id = id).first()
+            return make_response(scientist.to_dict(), 200)
+        except:
+            return make_response({"error": "Scientist not found"}, 404)
+    
+    def patch(self, id):
+        try:
+            scientist = Scientist.query.filter_by(id = id).first()
+            data = request.get_json()
+            for attr in data:
+                setattr(scientist, attr, data[attr])
+            db.session.commit()
+            return make_response(scientist.to_dict(),202)
+        except AttributeError:
+            return make_response({"error": "Scientist not found"}, 404)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+        except Exception as e:
+            return make_response({'error':f'wtf did you do {e}'}, 420)
+
+    def delete(self, id):
+        from sqlalchemy.orm.exc import UnmappedInstanceError
+        try:
+            scientist = Scientist.query.filter_by(id = id).first()
+            db.session.delete(scientist)
+            db.session.commit()
+            return make_response({},204)
+        except UnmappedInstanceError:
+            return make_response({"error": "Scientist not found"}, 404)
+
+api.add_resource(ScientistsById, '/scientists/<int:id>')
+
+
+
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -26,35 +26,73 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='planet', cascade = 'all, delete-orphan')
+    scientists = association_proxy('missions', 'scientist')
 
     # Add serialization rules
+    serialize_rules = ('-missions', )
 
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    field_of_study = db.Column(db.String)
+    name = db.Column(db.String, nullable = False)
+    field_of_study = db.Column(db.String, nullable = False)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='scientist', cascade = 'all, delete-orphan')
+    planets = association_proxy('missions', 'planet')
 
     # Add serialization rules
+    serialize_rules = ('-missions.scientist',)
 
     # Add validation
+    @validates('name')
+    def validate_name(self, key, new_name):
+        if not new_name:
+            raise ValueError('Scientist much have a name')
+        return new_name
 
+    @validates('field_of_study')
+    def validate_field_of_study(self, key, new_field_of_study):
+        if not new_field_of_study:
+            raise ValueError('Scientist much have a field_of_study')
+        return new_field_of_study
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable = False)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'), nullable = False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable = False)
 
     # Add relationships
+    scientist = db.relationship('Scientist', back_populates='missions')
+    planet = db.relationship('Planet', back_populates='missions')
 
     # Add serialization rules
+    serialize_rules = ('-scientist.missions', '-planet.missions')
 
     # Add validation
+    @validates('name')
+    def validate_name(self, key, new_name):
+        if not new_name:
+            raise ValueError('Mission much have a name')
+        return new_name
+    
+    @validates('scientist_id')
+    def validate_scientist_id(self, key, new_scientist_id):
+        if not new_scientist_id:
+            raise ValueError('Mission much have a scientist_id')
+        return new_scientist_id
+    
+    @validates('planet_id')
+    def validate_planet_id(self, key, new_planet_id):
+        if not new_planet_id:
+            raise ValueError('Mission much have a planet_id')
+        return new_planet_id
 
 
 # add any models you may need.
